d9038138891b3508210f8f6e754cced97d1fa510
Chetan Shivashankar <chetan.shivashankar@yara.com>
2022-01-01 09:51:37 +0530

    Removing non broker data

--- src/global_config/brokers.py
+++ src/global_config/brokers.py
@@ -4,8 +4,6 @@ BROKERS = {
         "mfa_url": "https://kite.zerodha.com/api/twofa",
         "request_token_url": "https://kite.trade/connect/login?api_key=",
         "redirect_url": "",
-        "mfa": "123789",
-        "user": "IGK017",
         "region": "ap-south-1",
         "login_timeout": "7",
         "name": "zerodha"
